aefb3fc14e8c9efdd812b6511a5476aec9502e39
StevePro7 <steveprozune@hotmail.com>
2021-04-12 20:09:54 +0100

    complete module

--- app/cmd/gradingservice/main.go
+++ app/cmd/gradingservice/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"app/grades"
+	"app/log"
 	"app/registry"
 	"app/service"
 	"context"
@@ -16,6 +17,8 @@ func main() {
 	var r registry.Registration
 	r.ServiceName = registry.GradingService
 	r.ServiceURL = serviceAddress
+	r.RequiredServices = []registry.ServiceName{registry.LogService}
+	r.ServiceUpdateURL = r.ServiceURL + "/services"
 
 	ctx, err := service.Start(context.Background(),
 		host,
@@ -24,6 +27,12 @@ func main() {
 		grades.RegisterHandlers)
 	if err != nil {
 		stlog.Fatal(err)
+	}
+	if logProvider, err := registry.GetProvider(registry.LogService); err == nil {
+		fmt.Printf("Logging service found at: %v\n", logProvider)
+		log.SetClientLogger(logProvider, r.ServiceName)
+	} else {
+		stlog.Println(err)
 	}
 
 	<-ctx.Done()
